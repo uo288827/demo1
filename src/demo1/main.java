8fee122829f7123107f9a369bfe5c2ec303310ef
Personal <Personal@LAPTOP-97RNRAMO>
2025-02-14 19:03:39 +0100

    Mejoras en Main.java - combinados tres cambios en uno

--- src/demo1/main.java
+++ src/demo1/main.java
@@ -3,8 +3,14 @@ package demo1;
 public class main {
 	public static void main(String[] args) {
 		System.out.print("Hola");
-		System.out.print("Cambioos en Eclipse");
-		System.out.print("Archivo editado desde GitHub");
+		System.out.println("Cambioos en Eclipse");
+		System.out.println("marta te quiere mucho");
+		System.out.println("Archivo editado desde GitHub");
+		System.out.println("primer cambio");
+		System.out.println("segundo cambio");
+		System.out.println("tercer cambio");
+
+
 	}
 
-}
+} 
